a1519fed8accd52246c2cf47bcf574a42b88326c
imnyang <me@imnya.ng>
2025-06-27 18:42:08 +0900

    hardware-configuration.nix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-cp /etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
+ln -s /etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
 sudo nixos-rebuild switch --flake .#default
